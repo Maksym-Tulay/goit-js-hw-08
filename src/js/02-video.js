ed1e1f0b7ca4d8900f34d63e579d10f836e6744f
mm_tulay <109350207+Maksym-Tulay@users.noreply.github.com>
2023-01-04 11:36:11 +0200

    task--02__done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+// adding a lodash throttle
 
+import throttle from 'lodash.throttle';
+
+// adding refs
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+// adding player eventlistener
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// adding function to save vplayer current time
+
+function onPlay({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+}
+
+// adding method to start player from the previous time
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
