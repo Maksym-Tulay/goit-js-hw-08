d21c7fda39762e58bce9b0e93e9f71871b952c54
mm_tulay <109350207+Maksym-Tulay@users.noreply.github.com>
2023-01-04 09:52:07 +0200

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,10 @@
+// import SimpleLightbox from "simplelightbox";
+
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log("Hello World");
