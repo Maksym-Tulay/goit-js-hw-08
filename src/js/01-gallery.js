ea52832f86198e71cb1671006ecc56878a4ef505
mm_tulay <109350207+Maksym-Tulay@users.noreply.github.com>
2023-01-04 10:25:12 +0200

    task--01__done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,36 @@
-// import SimpleLightbox from "simplelightbox";
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
 
-console.log("Hello World");
+// refs
+
+const galleryRefs = document.querySelector('.gallery');
+
+// function for creating gallery
+
+function createImageGallery() {
+  const galleryMarkup = galleryItems.reduce((acc, { original, preview, description }) => acc + 
+    `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+    '');
+
+  galleryRefs.insertAdjacentHTML('afterbegin', galleryMarkup);
+};
+
+// creating of a gallery
+
+createImageGallery();
+
+// opening lightbox
+
+const lightbox = new SimpleLightbox('.gallery a',
+  {
+    captionsData: "alt",
+    captionDelay: 250,
+  }
+);
